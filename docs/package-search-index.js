a97cdc0fee92e99a61646a7d486d227faac4968b
William Barkoff <william@barkoffusa.com>
2019-10-03 11:14:20 -0400

    add docs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.williambarkoff.csx.riemann"},{"l":"com.williambarkoff.csx.rustremoval"},{"l":"com.williambarkoff.csx.rustremoval.quadratics"}]
